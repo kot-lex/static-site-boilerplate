454efcbf9c4ba5803da94c75091de0472cfc818f
Aleksei Chernov <kot.lex@gmail.com>
2017-04-01 22:29:03 +0300

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -20,5 +21,9 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/templates/index.html'
+    })
   ]
 };
